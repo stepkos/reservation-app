d607076926dcf646f06ea36ab08b2f5c2d7506ae
R3VANEK <janek2000-2000@wp.pl>
2021-10-31 23:31:20 +0100

    base seeds for db
    added seeds that inserts not-random pre-set data in tables visit_types and roles. These rows must not be random factored

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \DB::table('roles')->insert(['id' => 1,'role' => 'Patient']);
+        \DB::table('roles')->insert(['id' => 2,'role' => 'Doctor']);
+
+
+        $visit_types = [
+            [
+                'type' => 'Konsultacja',
+                'standard_duration' => 1
+            ],[
+                'type' => 'Wstawianie plomby',
+                'standard_duration' => 1
+            ],[
+                'type' => 'Hemisekcja',
+                'standard_duration' => 2
+            ],[
+                'type' => 'Kortykotomia',
+                'standard_duration' => 3
+            ],[
+                'type' => 'Skaling',
+                'standard_duration' => 1
+            ],[
+                'type' => 'Lakowanie',
+                'standard_duration' => 1
+            ]
+        ];
+
+
+        \DB::table('visit_types')->insert($visit_types);
+
+        
+
         // \App\Models\User::factory(10)->create();
     }
 }
