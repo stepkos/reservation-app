e2c0216997dc1bd551ec82c89f25df47e427f089
R3VANEK <janek2000-2000@wp.pl>
2021-11-14 14:19:55 +0100

    fixed user role ratio in seeder

--- database/factories/User_dataFactory.php
+++ database/factories/User_dataFactory.php
@@ -13,9 +13,18 @@ class User_dataFactory extends Factory
      */
     public function definition()
     {
+
+        $random_number = rand(1,100);
+        if($random_number < 70)
+            $role_id = 1;
+        else if($random_number > 70 && $random_number < 90)
+            $role_id = 2;
+        else
+            $role_id = 3;
+
         return [
             'phone' => $this->faker->numerify('###-###-###'),
-            'role_id' => rand(1,3)
+            'role_id' =>$role_id
         ];
     }
 }
