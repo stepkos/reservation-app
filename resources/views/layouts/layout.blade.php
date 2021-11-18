cdef8b70311fb72ad1b7b763851ab6b02a47238e
R3VANEK <janek2000-2000@wp.pl>
2021-11-18 13:28:34 +0100

    refactored styling to patient main panel

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,11 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- css imports -->
     <link href="{{ asset('css/panel1.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main_patient.css') }}" rel="stylesheet">
+    <!-------------------------------------------------------------->
+
+    <!-- font imports -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <!-------------------------------------------------------------->
+
+    <!-- https://www.npmjs.com/package/color-calendar#usage-basic -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/color-calendar/dist/css/theme-basic.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/color-calendar/dist/css/theme-glass.css" />
+    <script src="https://cdn.jsdelivr.net/npm/color-calendar/dist/bundle.min.js"></script>
+    <!--------------------------------------------------------------->
+    
     <title>@yield('title')</title>
 </head>
 <body>
 
+
+    
 
     <nav>
         <div id="logo_box"></div>
--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -7,11 +7,30 @@
 
 @section('content')
     <section id="visits_holder">
-        
+        <article class="visit_card"></article>
+        <article class="visit_card"></article>
+        <article class="visit_card"></article>
+        <article class="visit_card"></article>
+        <article class="visit_card"></article>  
     </section>
 
 
+    <section id="calendar_holder">
+        <div id="color-calendar"></div>
+        <article class="calendar_event"></article>
+        <article class="calendar_event"></article>
+        <article class="calendar_event"></article>
+    </section>
 
+
+    <script language="javascript">
+        new Calendar({
+            id: '#color-calendar',
+            primaryColor:"#37aee6",
+            theme:"glass",
+            headerBackgroundColor:"#37aee6"
+        })
+    </script>
 
     
 @endsection
