9326c049aedd2dc9fe2c97454d30b494b8adcb43
ShamanDev <pivotguy888@gmail.com>
2021-11-26 00:50:35 +0100

    Created content and css for patient_make_appointment panel

--- resources/views/patient_make_appointment.blade.php
+++ resources/views/patient_make_appointment.blade.php
@@ -4,5 +4,23 @@
 
 
 @section('content')
-Tu będzie formularz
+<div id="appointment_form_holder">
+    <div id="appointment_banner">
+        Umawianie wizyty
+    </div>
+    <form id="appointment_form">
+        <select id="form_doctor" name="doctor_name">
+            <option>Johnny Sins</option>
+            <option>Jerzy Nowak</option>
+            <option>Andrzej Andrzej</option>
+        </select>
+        <select id="form_doctor" name="visit_type">
+            <option>Wyrywanie zęba</option>
+            <option>Leczenie kanałowe</option>
+            <option>Wstawienie plomby</option>
+        </select>
+        <textarea id="form_description" placeholder="Krótki opis dla lekarza..." name="short_description"></textarea>
+        <input type="submit" />
+    </form>
+</div>
 @endsection
