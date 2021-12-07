1e1f9f17c366dca31095ed457fa929704663dbf6
R3VANEK <janek2000-2000@wp.pl>
2021-12-07 17:32:23 +0100

    now seeding with archive visits (-1 year, + 1 year)

--- database/factories/VisitFactory.php
+++ database/factories/VisitFactory.php
@@ -25,7 +25,7 @@ class VisitFactory extends Factory
             'patient_id' => $patients[rand(0, count($patients)-1)]->id,
             'doctor_id' => $doctors[rand(0, count($doctors)-1)]->id,
             'visit_type_id' => rand(1, $visit_type_count),
-            'date' => $this->faker->dateTimeBetween('+0 days', '+2 years'),
+            'date' => $this->faker->dateTimeBetween('-1 year', '+1 year'),
             'description' => $this->faker->paragraph(),
             'approved_by_reception' => rand(0, 1) == 1
         ];
