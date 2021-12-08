c56beeb629b58e19b7cd35b6588eefffb2240f84
DΣUS <76174125+GodsCrisis@users.noreply.github.com>
2021-12-08 15:53:41 +0100

    Add files via upload

--- resources/views/patient/add_appointment.blade.php
+++ resources/views/patient/add_appointment.blade.php
@@ -9,20 +9,38 @@
         Umawianie wizyty
     </div>
     <form id="appointment_form">
-        <select id="form_doctor" name="doctor_name">
-            @foreach($doctors as $doctor)
-                <option value="{{ $doctor->id }}">{{ $doctor->name }}</option>
-            @endforeach
-        </select>
-        <select id="form_doctor" name="visit_type">
-            @foreach($visitTypes as $visitType)
-                <option value="{{ $visitType->id }}">{{ $visitType->type }}</option>
-            @endforeach
-        </select>
-        <textarea id="form_description" placeholder="Krótki opis dla lekarza..." name="short_description"></textarea>
-        <input type="date" name="visit_date" />
-        <input type="time" name="visit_time" />
-        <input type="submit" />
+        <div id="form_down">
+            <select id="form_doctor" name="doctor_name">
+                @foreach($doctors as $doctor)
+                    <option value="{{ $doctor->id }}">{{ $doctor->name }}</option>
+                @endforeach
+            </select>
+            <select id="form_doctor" name="visit_type">
+                @foreach($visitTypes as $visitType)
+                    <option value="{{ $visitType->id }}">{{ $visitType->type }}</option>
+                @endforeach
+            </select>
+            <textarea id="form_description" placeholder="Krótki opis dla lekarza..." name="short_description"></textarea>
+            <input type="date" name="visit_date" />
+            <input type="time" name="visit_time" />
+            <input type="submit" />
+            <span id="error">Error ci wyjebało</span>
+        </div>
+        
+        <div id="appointment_work_hours">
+            <span>Godziny pracy</span>
+            <span id="doctor" class="hours">Marian Kowalski</span>
+            <span class="day">Poniedziałek</span>
+            <span class="hours">8:00-16:00</span>
+            <span class="day">Wtorek</span>
+            <span class="hours">8:00-16:00</span>
+            <span class="day">Środa</span>
+            <span class="hours">8:00-16:00</span>
+            <span class="day">Czwartek</span>
+            <span class="hours">8:00-16:00</span>
+            <span class="day">Piątek</span>
+            <span class="hours">8:00-16:00</span>
+        </div>
     </form>
 </div>
 @endsection
