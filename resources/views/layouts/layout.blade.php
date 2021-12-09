68f398e426acce3448b0bccb1bb3c23b9e210dda
stepkos <stepkos@wp.pl>
2021-12-09 23:21:30 +0100

    new imports

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -13,7 +13,7 @@
     <!-- font imports -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     <!-------------------------------------------------------------->
 
     <!-- https://www.npmjs.com/package/color-calendar#usage-basic -->
