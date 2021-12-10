1e190dddd97e0f2e360ed8bc2ebee49af3b2431c
DΣUS <76174125+GodsCrisis@users.noreply.github.com>
2021-12-10 16:26:01 +0100

    Add files via upload

--- resources/views/doctor/no-visit.blade.php
+++ resources/views/doctor/no-visit.blade.php
@@ -5,7 +5,29 @@
 
 @section('content')
 
-    <h1>Siema nie masz wizyty typie</h1>
+    <div id="no-visit-holder">
+        <div id="no-visit">
+            <span>Brak aktualnej wizyty</span>
+        </div>
+        <div id="next-visit">
+            <span>Następna wizyta</span>
+        </div>
+        <div id="no-visit-next-visit">
+            <div id="next-visit-hour">
+                <span class="up">Godzina</span>
+                <span class="down">8:00</span>
+            </div>
+            <div id="next-visit-name">
+                <span class="up">Imię i nazwisko pacjenta</span>
+                <span class="down">Jan Kowalski Jr.</span>
+            </div>
+            <div id="next-visit-type">
+                <span class="up">typ wizyty</span>
+                <span class="down">Plomba</span>
+            </div>
+        </div>
+
+    </div>
 
 
 @endsection
