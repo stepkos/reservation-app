7ed996858fcb0334009ff5eaf7a1a0f62e3cb5c1
stepkos <stepkos@wp.pl>
2021-12-11 23:31:00 +0100

    panel1 -> panel

--- resources/views/layouts/layout_reception.blade.php
+++ resources/views/layouts/layout_reception.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- css imports -->
-    <link href="{{ asset('css/panel1.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/panel.css') }}" rel="stylesheet">
     <link href="{{ asset('css/reception.css') }}" rel="stylesheet">
     @yield('extra_css')
     <!-------------------------------------------------------------->
