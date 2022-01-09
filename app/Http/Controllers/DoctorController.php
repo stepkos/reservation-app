3e92cd7f9609d6d281740a9dc34d8fbadca07338
R3VANEK <janek2000-2000@wp.pl>
2022-01-09 21:17:36 +0100

    added .phar file, added comments to DoctorController

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * DoctorController.php
+ * @author    Jakub Stępkowski <stepkos@example.com>>
+ * @copyright 2021 CrazyDevelopers
+ * @license   All rights reserved
+ * @see       https://github.com/stepkos/ReservationApp
+ * @version   1.0.0
+ */
+
 
 namespace App\Http\Controllers;
 
@@ -6,14 +15,32 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Visit;
 
+
+
+
 class DoctorController extends Controller
 {
+
+    /**
+     * Gets future visits of doctor and renders its home pannel
+     *
+     * @param null
+     *
+     * @return View
+     */
     public function get_home() {
 
         $visits = User::allFutureVisits(auth()->user()->id);
         return view("doctor.home", compact('visits'));
     }
 
+    /**
+     * Gets future and archive visits of doctor and renders its home_archive pannel
+     *
+     * @param null
+     *
+     * @return View
+     */
     public function get_home_archive() {
 
         $visits_archive = User::allArchiveVisits(auth()->user()->id);
@@ -21,6 +48,14 @@ class DoctorController extends Controller
         return view("doctor.home_archive", compact(['visits_archive', 'visits_actual']));
     }
 
+    /**
+     * Shows current visit and data related to it
+     * If no current visit is being held at the moment, shows time of next visit
+     *
+     * @param null
+     *
+     * @return View
+     */
     public function get_visit() {
         $visit = User::currentVisit(auth()->user()->id);
 
@@ -33,6 +68,13 @@ class DoctorController extends Controller
         return view("doctor.no-visit", compact('nextVisit'));
     }
 
+    /**
+     * Post method for visit_card_view
+     *
+     * @param null
+     *
+     * @return View
+     */
     public function post_visit(Request $request) {
         Visit::find($request->get('visit_id'))->update([
             'description' => $request->get('desc')
